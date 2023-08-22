4d81b9ca3e9d8586b57c42a781d6897ecbe43d81
Ayoub Ghriss <dev@aghriss.net>
2023-08-22 08:07:59 +0200

    added linking script & new structure

--- link.sh
+++ link.sh
@@ -1,19 +1,49 @@
 #!/bin/bash
 
-dot=$1
-SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
+CFG="$1"
+SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
 
-prepare(){
- local src="$1"
-}
-
-case $dot in
+case $CFG in
+    alacritty)
+        src=("$SCRIPT_DIR/alacritty")
+        trg=("$HOME/.config/alacritty")
+    ;;
+    i3)
+        src=("$SCRIPT_DIR/i3")
+        trg=("$HOME/.config/i3")
+    ;;
+    ideavim)
+        src=("$SCRIPT_DIR/ideavim")
+        trg=("$HOME/.ideavimrc")
+    ;;
     neovim)
-        src="nvim"
-        ;;
+        src=("$SCRIPT_DIR/nvim")
+        trg=("$HOME/.config/nvim")
+    ;;
+    picom)
+        src=("$SCRIPT_DIR/picom.conf")
+        trg=("$HOME/.config/picom.conf")
+    ;;
+    polybar)
+        src=("$SCRIPT_DIR/polybar")
+        trg=("$HOME/.config/polybar")
+    ;;
+    tmux)
+        src=("$SCRIPT_DIR/tmux")
+        trg=("$HOME/.config/tmux")
+    ;;
+    zathura)
+        src=("$SCRIPT_DIR/zathura")
+        trg=("$HOME/.config/zathura")
+    ;; 
+    zsh)
+        src=("$SCRIPT_DIR/zshell/zshrc" "$SCRIPT_DIR/zshell/zsh")
+        trg=("$HOME/.zshrc" "$HOME/.config/zsh" )
+    ;;
 esac
 
-target=$SCRIPT_DIR/
-
-
-
+# target=("$SCRIPT_DIR/")
+for i in "${!src[@]}"; do
+    echo Linking "${src[$i]}" to "${trg[$i]}"
+    ln -s "${src[$i]}" "${trg[$i]}"
+done
---
+++ nvim/lua/aghriss/scratch/mason.lua
@@ -0,0 +1,9 @@
+-- local opts = require("aghriss.plugins.options.mason")
+-- require("base46").load_highlights("mason")
+-- require("mason").setup(opts)
+
+-- custom nvchad cmd to install all mason binaries listed
+-- vim.api.nvim_create_user_command("MasonInstallAll", function()
+	-- vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+-- end, {})
+-- vim.g.mason_binaries_list = opts.ensure_installed
