16594242aba7b996ebcadb8a5f7fe22afa1ac6d8
Ayoub Ghriss <dev@aghriss.net>
2023-08-22 17:26:32 +0200

    add xsessions and hibernate scripts

--- scripts/hibernate.sh
+++ scripts/hibernate.sh
@@ -6,12 +6,12 @@ case $1/$2 in
   pre/*)
     # Put here any commands you want to be run when suspending or hibernating.
     # echo "test from /lib64/elogind/system-sleep/" >> /var/log/elogin.log
-    # echo 1,2 = "$1", "$2" >> /var/log/elogin.log
+    echo 1,2 = "$1", "$2" >> /var/log/elogin.log
     # enable touchpad before hibernate (I use i3+plasma)
-    echo "Enabling touchbad before $1 $2" >>  /var/log/elogind.log
-    sudo -u aghriss -g aghriss -- qdbus org.kde.keyboard /modules/kded_touchpad org.kde.touchpad.enable 2>&1 | tee -a /var/log/elogind.log
-    status="$(sudo -u aghriss -g aghriss -- qdbus org.kde.keyboard /modules/kded_touchpad org.kde.touchpad.isEnabled)"
-    echo "Touchpad is now enabled: $status" >> /var/log/elogind.log
+    # echo "Enabling touchpad before $1 $2" >>  /var/log/elogind.log
+    # sudo -u aghriss -g aghriss -- qdbus org.kde.keyboard /modules/kded_touchpad org.kde.touchpad.enable 2>&1 | tee -a /var/log/elogind.log
+    # status="$(sudo -u aghriss -g aghriss -- qdbus org.kde.keyboard /modules/kded_touchpad org.kde.touchpad.isEnabled)"
+    # echo "Touchpad is now enabled: $status" >> /var/log/elogind.log
     ;;
   post/*)
     # Put here any commands you want to be run when resuming from suspension or thawing from hibernation.
